aecca3a0574a99d00c841fc284a518cbbb440706
Alvaro Martinez <alvaromarsa1985@gmail.com>
2025-11-26 12:15:41 +0100

    Barra de navegacion lateral agragada

--- OneDrive/Escritorio/Angular/MHWIB/src/app/app.routes.ts
+++ OneDrive/Escritorio/Angular/MHWIB/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { MonsterViewComponent } from './components/monster-view/monster-view.component';
+import { ArmorViewComponent } from './components/armor-view/armor-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '',
+    component: MenuComponent,
+  },
+  { path: 'monsterViewComponent',
+    component: MonsterViewComponent,
+  },
+  { path: 'armorViewComponent',
+    component: ArmorViewComponent,
+  },
+
+];
--- OneDrive/Escritorio/Angular/MHWIB/src/app/components/navbar/navbar.component.ts
+++ OneDrive/Escritorio/Angular/MHWIB/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import {  Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent { }
